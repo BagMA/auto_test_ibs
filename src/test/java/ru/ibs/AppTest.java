b528d7de375801785de9741cd229c9dcda5fcf31
Michael Bagin <bagin_mihail@cybersec74.ru>
2023-08-23 10:23:22 +0500

    добавил поиск по xpath, изменил версию junit, добавил window.maximize

--- src/test/java/ru/ibs/AppTest.java
+++ src/test/java/ru/ibs/AppTest.java
@@ -1,9 +1,6 @@
 package ru.ibs;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,52 +9,51 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest
 {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
 
 
-    @Before
-    public void setUp()
+    @BeforeClass
+    public static void setUp()
     {
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 3);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.navigate().to("http://localhost:8080/");
+        driver.manage().window().maximize();
     }
 
-    @After
-    public void tearDown()
+    @AfterClass
+    public static void tearDown()
     {
         driver.quit();
     }
 
     @Test
     public void testAddVegetable() throws InterruptedException {
-        driver.navigate().to("http://localhost:8080/");
         driver.findElement(By.id("navbarDropdown")).click();
-        driver.findElement(By.cssSelector(".dropdown-item[href='/food']")).click();
-        driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+        driver.findElement(By.xpath("//div/a[@href='/food']")).click();
+        driver.findElement(By.xpath("(//button[@class='btn btn-primary'])[1]")).click();
         driver.findElement(By.id("name")).sendKeys("Картофель");
         driver.findElement(By.id("save")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")));
+        wait.until(ExpectedConditions.
+                visibilityOfElementLocated(By.xpath("//tr[5]/td[1]")));
         Assert.assertTrue("картофель не найден",
-                driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")).isDisplayed());
-        Assert.assertEquals("Картофель",
-                driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")).getText());
-        Assert.assertEquals("Овощ",
-                driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:nth-of-type(2)")).getText());
-        Assert.assertEquals("false",
-                driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:nth-of-type(3)")).getText());
+                driver.findElement(By.xpath("//tr[5]/td[1]")).isDisplayed());
+        Assert.assertEquals("не картофель", "Картофель",
+                driver.findElement(By.xpath("//tr[5]/td[1]")).getText());
+        Assert.assertEquals("не овощ","Овощ",
+                driver.findElement(By.xpath("//tr[5]/td[2]")).getText());
+        Assert.assertEquals("не false","false",
+                driver.findElement(By.xpath("//tr[5]/td[3]")).getText());
     }
 
     @Test
     public void testAddFruit() {
-        driver.navigate().to("http://localhost:8080/");
         driver.findElement(By.id("navbarDropdown")).click();
         driver.findElement(By.cssSelector(".dropdown-item[href='/food']")).click();
         driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
@@ -69,11 +65,11 @@ public class AppTest
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")));
         Assert.assertTrue("ананас не найден",
                 driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")).isDisplayed());
-        Assert.assertEquals("Ананас",
+        Assert.assertEquals("не ананас","Ананас",
                 driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:first-of-type")).getText());
-        Assert.assertEquals("Фрукт",
+        Assert.assertEquals("не фрукт", "Фрукт",
                 driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:nth-of-type(2)")).getText());
-        Assert.assertEquals("true",
+        Assert.assertEquals("не true", "true",
                 driver.findElement(By.cssSelector("tbody > tr:last-of-type > td:nth-of-type(3)")).getText());
     }
 }
